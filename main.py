fae30d28dd013e8dc68a342895dde8b4361fb5ad
jagyasenbhatra <bhatrajagyasen@gmail.com>
2024-12-02 16:40:03 +0530

    fastapi:v2

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ app = FastAPI()
 def calculate_square(x):
     return x * x
 
+def calculate_cube(x):
+    return x*x*x
+
 @app.get("/square/{number}")
 async def square(number: int):
     return {"result": calculate_square(number)}
+
+
+@app.get("/cube/{number}")
+async def cube(number: int):
+    return {"result": calculate_cube(number)}
